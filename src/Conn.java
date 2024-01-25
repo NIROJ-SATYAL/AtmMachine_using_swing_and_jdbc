cd6f14516d86865cf3798dccc58d26855d5dbdf6
niroj satyal <niroj.satyal2000@gmail.com>
2024-01-25 09:40:45 +0545

    added connection class

---
+++ src/Conn.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+public class    Conn {
+    Connection c;
+
+
+    public Conn() {
+        try {
+
+//            step 1:register the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver load successfully...");
+//                    step2 :create connection
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/AtmMachine","root","jorin!@#1");
+            System.out.println("connection created successfully...");
+
+
+
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Conn conn=new Conn();
+    }
+}
